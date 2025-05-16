b70c449009030addf459bd4d9f64d8eddded1031
ВашNikitinRadmirgit config --global user.email RANikitin@stud.kpfu.rugit config --global user.password ваш_ÐnrA2284856 <radmir.nikitin8@gmail.com>
2025-05-16 23:59:37 +0300

    fix

---
+++ SocialNetwork/Controllers/MessangerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Elomoas.Models;
+
+
+namespace Elomoas.Controllers
+{
+    public class MessangerController : Controller
+    {
+        private readonly ILogger<MessangerController> _logger;
+        private readonly IMediator _mediator;
+
+        public MessangerController(ILogger<MessangerController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Messanger()
+        {
+
+            return View();
+        }
+        
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
